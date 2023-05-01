e9f0565a39d4c4e08297f6c84bd6d043a4be45ce
jerikchan <jerikchan@gmail.com>
2023-05-02 02:13:03 +0800

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,26 @@
+class CommentsController < ApplicationController
+
+  http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+  
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.build(comment_params)
+    if @comment.save
+      redirect_to @article
+    else
+      render 'articles/show', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article), status: :see_other
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body, :status)
+    end
+end
